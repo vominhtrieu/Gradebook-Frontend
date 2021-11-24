d1fba8734c6d338b954f3fe0126b25773be65b65
vominhtrieu <minhtrieuvo600@gmail.com>
2021-11-24 20:50:44 +0700

    Minor fixes

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -7,92 +7,85 @@ import userEmailRules from "../../form-rules/userEmail";
 import userPasswordRules from "../../form-rules/userPassword";
 import GoogleLogin from "react-google-login";
 import {
-  googleSignInSuccessHandler,
-  googleSignInFailureHandler,
-  signIn,
+    googleSignInSuccessHandler,
+    googleSignInFailureHandler,
+    signIn,
 } from "../../handlers/signIn";
 import { GOOGLE_CLIENT_ID } from "../../configs/auth";
 
 function SignIn() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [loginIsSuccessful, setLoginIsSuccessful] = useState<boolean>(false);
-  const [form] = Form.useForm();
-  const history = useHistory();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [loginIsSuccessful, setLoginIsSuccessful] = useState<boolean>(false);
+    const [form] = Form.useForm();
+    const history = useHistory();
 
-  useEffect(() => {
-    localStorage.removeItem("token");
-  }, []);
+    useEffect(() => {
+        localStorage.removeItem("token");
+    }, []);
 
-  useEffect(() => {
-    if (loginIsSuccessful) {
-      const studentId = localStorage.getItem("studentId");
-      localStorage.removeItem("studentId");
+    useEffect(() => {
+        if (loginIsSuccessful) {
+            history.push("/");
+        }
+    }, [loginIsSuccessful, history]);
 
-      if (studentId === "null") {
-        history.push("/profile/studentId");
-      } else {
-        history.push("/");
-      }
-    }
-  }, [loginIsSuccessful, history]);
+    const callSignIn = () => {
+        signIn(setLoading, setLoginIsSuccessful, form);
+    };
 
-  const callSignIn = () => {
-    signIn(setLoading, setLoginIsSuccessful, form);
-  };
+    const callGoogleSignInSuccess = (res: any) => {
+        googleSignInSuccessHandler(setLoading, setLoginIsSuccessful, res);
+    };
 
-  const callGoogleSignInSuccess = (res: any) => {
-    googleSignInSuccessHandler(setLoading, setLoginIsSuccessful, res);
-  };
-
-  return (
-    <Card title="Sign In" className="auth-form">
-      <Form form={form} layout="vertical">
-        <Form.Item name="email" rules={userEmailRules}>
-          <Input
-            type="email"
-            prefix={<MailOutlined className="site-form-item-icon" />}
-            placeholder="Email"
-          />
-        </Form.Item>
-        <Form.Item name="password" rules={userPasswordRules}>
-          <Input
-            type="password"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button onClick={callSignIn} disabled={loading} type="primary" block>
-            {loading ? <Spin style={{ paddingRight: 5 }} /> : null}Sign In
-          </Button>
-        </Form.Item>
-        <Divider>Or</Divider>
-        <Form.Item>
-          <GoogleLogin
-            clientId={GOOGLE_CLIENT_ID}
-            render={renderProps => (
-              <Button
-                type="default"
-                icon={<GoogleOutlined />}
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                block
-              >
-                Sign In with Google
-              </Button>
-            )}
-            onSuccess={callGoogleSignInSuccess}
-            onFailure={googleSignInFailureHandler}
-            cookiePolicy={"single_host_origin"}
-          />
-        </Form.Item>
-        <Divider>Or</Divider>
-        <Form.Item style={{ textAlign: "center", marginBottom: 0 }}>
-          <Link to="/signup">Create a new account</Link>
-        </Form.Item>
-      </Form>
-    </Card>
-  );
+    return (
+        <Card title="Sign In" className="auth-form">
+            <Form form={form} layout="vertical">
+                <Form.Item name="email" rules={userEmailRules}>
+                    <Input
+                        type="email"
+                        prefix={<MailOutlined className="site-form-item-icon" />}
+                        placeholder="Email"
+                    />
+                </Form.Item>
+                <Form.Item name="password" rules={userPasswordRules}>
+                    <Input
+                        type="password"
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Button onClick={callSignIn} disabled={loading} type="primary" block>
+                        {loading ? <Spin style={{paddingRight: 5}} /> : null}Sign In
+                    </Button>
+                </Form.Item>
+                <Divider>Or</Divider>
+                <Form.Item>
+                    <GoogleLogin
+                        clientId={GOOGLE_CLIENT_ID}
+                        render={renderProps => (
+                            <Button
+                                type="default"
+                                icon={<GoogleOutlined />}
+                                onClick={renderProps.onClick}
+                                disabled={renderProps.disabled}
+                                block
+                            >
+                                Sign In with Google
+                            </Button>
+                        )}
+                        onSuccess={callGoogleSignInSuccess}
+                        onFailure={googleSignInFailureHandler}
+                        cookiePolicy={"single_host_origin"}
+                    />
+                </Form.Item>
+                <Divider>Or</Divider>
+                <Form.Item style={{textAlign: "center", marginBottom: 0}}>
+                    <Link to="/signup">Create a new account</Link>
+                </Form.Item>
+            </Form>
+        </Card>
+    );
 // =======
 // import {Form, Input, Button, Card, Spin, message} from "antd";
 // import {MailOutlined, LockOutlined} from "@ant-design/icons";
