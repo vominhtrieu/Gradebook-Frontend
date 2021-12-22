6f14b23e0682c476b5be6d413cfae94763616e9b
hoangtrung-0722 <hoangtrung.220720@gmail.com>
2021-12-22 20:15:01 +0700

    fix load grade to gradeboard

--- src/components/class/grade_board/index.tsx
+++ src/components/class/grade_board/index.tsx
@@ -71,8 +71,8 @@ const data: DataSourceProps[] = [
 ]
 
 interface GradeBoardProps {
-  classId: number;
-  students: object[]
+    classId: number;
+    students: object[]
 }
 
 export default function GradeBoard({classId, students}: GradeBoardProps) {
@@ -99,8 +99,8 @@ export default function GradeBoard({classId, students}: GradeBoardProps) {
                 .then((gradeStructure: any) => {
                     gradeStructure.forEach((gradeItem: any, gradeStructureIndex: number) => {
                         getData(`/classrooms/${classId}/grade-board?gradeStructureId=${gradeItem.id}`).then((data) => {
-                            data.forEach((item: any) => {
-                                tempDataSource[gradeStructureIndex].grades.push(item.grade);
+                            data.forEach((item: any, index: number) => {
+                                tempDataSource[index + 1].grades.push(item.grade);
                             })
                         });
                         tempColumns.push({
@@ -122,14 +122,15 @@ export default function GradeBoard({classId, students}: GradeBoardProps) {
                                             studentId={record.name.studentId}
                                             gradeStructureId={gradeItem.id}
                                             classId={classId}
-                                            value={text[index - 1]}
+                                            value={tempDataSource[index].grades[gradeStructureIndex]}
                                         />
                                     );
                                 }
                             },
                         });
-                    });
+                    })
                     setTimeout(() => {
+                        console.log(tempDataSource);
                         setGradeColumns(tempColumns);
                         setDataSource(tempDataSource);
                     }, 500);
