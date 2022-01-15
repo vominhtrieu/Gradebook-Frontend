53e9aab216543df8e92724abf8d65b8937cb4924
vominhtrieu <minhtrieuvo600@gmail.com>
2022-01-15 23:37:09 +0700

    Implement sort, search, change student id, add new admin

--- src/components/admin/UserManagement.tsx
+++ src/components/admin/UserManagement.tsx
@@ -1,34 +1,43 @@
 import { MinusOutlined } from '@ant-design/icons';
-import { Button, message, Table, } from 'antd';
+import { Button, Input, message, Table, } from 'antd';
 import React, { useEffect } from 'react';
 import { getData, postData } from "../../handlers/api";
+import ChangeStudentIDModal from "./ChangeStudentIDModal";
 
 
 export default function UserManagement() {
     const [users, setUsers] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
+    const [loading, setLoading] = React.useState(false);
+    const [isChangingStudentID, setIsChangingStudentID] = React.useState(false);
+    const [searchString, setSearchString] = React.useState<any>("");
+    const [lastSearchString, setLastSearchString] = React.useState<any>(null);
+    const [selectedRecord, setSelectedRecord] = React.useState(null);
 
     const blockUser = (record: any) => {
         postData("/admin/users/block", {
             id: record.id
         }).then(() => {
             message.success("Success");
-            setUsers((users:any)=>{
-                return users.map((user:any)=>user.id === record.id ?{...user, blocked: true}:user)
-            })
+            refresh();
         })
     }
-    console.log(users);
 
     const unBlockUser = (record: any) => {
         postData("/admin/users/unblock", {
             id: record.id
         }).then(() => {
             message.success("Success");
-            setUsers((users:any)=>{
-                return users.map((user:any)=>user.id === record.id ?{...user, blocked: false}:user)
-            })
+            refresh();
         })
+    }
+
+    const changeStudentID = (record: any) => {
+        setIsChangingStudentID(true);
+        setSelectedRecord(record);
+    }
+
+    const closeChangeStudentIDModal = () => {
+        setIsChangingStudentID(false);
     }
 
     const columns = [
@@ -73,23 +82,54 @@ export default function UserManagement() {
                                         blockUser(record);
                                     }}>Lock</Button>
                     }
-                    <Button>Change Student ID</Button>
+                    <Button onClick={() => changeStudentID(record)}>Change Student ID</Button>
                 </>
         }
     ];
 
-    useEffect(() => {
-        getData("/admin/users")
+    const refresh = () => {
+        setLoading(true);
+        getData("/admin/users?q=" + searchString)
             .then((data: any) => {
-                console.log(data);
+                setLastSearchString(searchString);
                 setLoading(false);
                 setUsers(data);
             })
             .catch(() => message.error("Something went wrong!"));
-    }, [])
+    }
+
+    useEffect(() => {
+        if (loading || searchString === lastSearchString) {
+            return;
+        }
+        setLoading(true);
+        getData("/admin/users?q=" + searchString)
+            .then((data: any) => {
+                setLastSearchString(searchString);
+                setLoading(false);
+                setUsers(data);
+            })
+            .catch(() => message.error("Something went wrong!"));
+    }, [loading, lastSearchString, searchString])
 
     return (
-        <Table dataSource={users} columns={columns} bordered
-               title={() => <h3 style={{padding: 0, margin: 0}}>User Management</h3>} loading={loading} />
+        <>
+            <ChangeStudentIDModal record={selectedRecord}
+                                  visible={isChangingStudentID}
+                                  onSuccess={() => {
+                                      refresh();
+                                      closeChangeStudentIDModal();
+                                  }}
+                                  onCancel={closeChangeStudentIDModal} />
+            <Table dataSource={users} columns={columns} bordered
+                   title={() => (
+                       <div style={{display: "flex"}}>
+                           <h3 style={{padding: 0, margin: 0}}>User Management</h3>
+                           <Input style={{marginLeft: "auto", height: 30, maxWidth: 300}} value={searchString}
+                                  onChange={(e) => setSearchString(e.target.value)}
+                                  placeholder="Search..." />
+                       </div>
+                   )} loading={loading} />
+        </>
     );
 };
