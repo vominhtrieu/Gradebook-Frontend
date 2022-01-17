f4395fbe6d49d22f4d76fd3d8f339372d1ad9a9d
hoangtrung-0722 <hoangtrung.220720@gmail.com>
2022-01-17 11:01:41 +0700

    export full gradeboard

--- src/components/class/grade_board/GradeBoardButtonContainer.tsx
+++ src/components/class/grade_board/GradeBoardButtonContainer.tsx
@@ -9,6 +9,8 @@ import {
 import GradeBoardDownloadMenu from "./GradeBoardDownloadMenu";
 import {useContext} from "react";
 import {MainContext} from "../../../contexts/main";
+import {getData} from "../../../handlers/api";
+import {exportFullGradeBoard} from "../../../handlers/exportDataHandler";
 
 interface GradeBoardButtonContainerProps {
     classId: number
@@ -35,6 +37,41 @@ export default function GradeBoardButtonContainer({classId, students}: GradeBoar
         }
     }
 
+    const handleExportGradeBoard = () => {
+        getData(`/classrooms/${classId}/grade-structures`).then((gradeStructure: any) => {
+            const gradeBoardData: object[] = [];
+            students.forEach((student: any) => {
+                gradeBoardData.push({
+                    studentId: student.studentId,
+                    studentName: student.name
+                });
+            })
+            const headers = [["MSSV", "Họ tên"]];
+            gradeStructure.reverse().forEach((gradeItem: any) => {
+                headers[0].push(gradeItem.name);
+                getData(`/classrooms/${classId}/grade-board?gradeStructureId=${gradeItem.id}`).then((studentGrades: any) => {
+                    const grades = Array(students.length);
+                    grades.fill("_");
+                        students.forEach((student: any, i: number) => {
+                            if (studentGrades) {
+                                const grade = studentGrades.find((e: any) => e.studentId === student.studentId);
+                                if (grade && grade.grade != null) {
+                                    gradeBoardData[i] = {...gradeBoardData[i], [gradeItem.name]: grade.grade.toString()};
+                                } else {
+                                    gradeBoardData[i] = {...gradeBoardData[i], [gradeItem.name]: "___"}
+                                }
+                            }
+                        })
+                })
+            })
+            setTimeout(() => {
+                exportFullGradeBoard(headers, gradeBoardData);
+            }, 100)
+        }).catch((err) => {
+            return message.error("Something went wrong");
+        })
+    }
+
     return (
         <div className="grade-board_button-container">
             <Dropdown overlay={menu} trigger={["click"]}>
@@ -54,7 +91,7 @@ export default function GradeBoardButtonContainer({classId, students}: GradeBoar
                 </Upload>
             </Tooltip>
             <Tooltip title="Export this grade board" placement="bottomLeft">
-                <Button type="text">
+                <Button type="text" onClick={handleExportGradeBoard}>
                     <ExportOutlined/> Export
                 </Button>
             </Tooltip>
