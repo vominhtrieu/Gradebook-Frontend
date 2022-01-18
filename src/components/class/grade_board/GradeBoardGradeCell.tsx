fe5e6882c6384247eede2a3be104a1cb0ed42721
TranHoangDat <tranhoangdat75@gmail.com>
2022-01-18 19:52:15 +0700

    Minor fix GradeBoardGradeCell

--- src/components/class/grade_board/GradeBoardGradeCell.tsx
+++ src/components/class/grade_board/GradeBoardGradeCell.tsx
@@ -1,109 +1,145 @@
-import React, { ChangeEvent, useContext, useRef, useState } from "react";
+import React, {
+  ChangeEvent,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { putData } from "../../../handlers/api";
 import { message } from "antd";
 import { MainContext } from "../../../contexts/main";
 
 interface InputProps extends React.HTMLProps<HTMLInputElement> {
-    classId?: number;
-    gradeStructureId?: number;
-    studentId?: string;
-    maximumGrade?: number;
+  classId?: number;
+  gradeStructureId?: number;
+  studentId?: string;
 }
 
 export default function GradeBoardGradeCell({
-                                                classId,
-                                                gradeStructureId,
-                                                studentId,
-                                                maximumGrade,
-                                                ...props
-                                            }: InputProps) {
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [grade, setGrade] = useState<string>(
-        props.value ? props.value.toString() : ""
-    );
-    const [focusInput, setFocusInput] = useState<boolean>(false);
-    const [metaDataWrapperState, setMetadataWrapperState] = useState<string>(
-        props.value ? " containing" : ""
-    );
-    const [isSaving, setIsSaving] = useState<boolean>(false);
-    const mainContext = useContext(MainContext);
+  classId,
+  gradeStructureId,
+  studentId,
+  ...props
+}: InputProps) {
+  const maximumGrade = 100;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [gradeStr, setGradeStr] = useState<string>(
+    props.value ? props.value.toString() : ""
+  );
+  const [grade, setGrade] = useState<string>(
+    props.value ? props.value.toString() : ""
+  );
+  const [focusInput, setFocusInput] = useState<boolean>(false);
+  const [metaDataWrapperState, setMetadataWrapperState] = useState<string>(
+    props.value ? " containing" : ""
+  );
+  const [isSaving, setIsSaving] = useState<boolean>(false);
+  const mainContext = useContext(MainContext);
 
-    const handleOnClickMetadataWrapper = () => {
-        setFocusInput(true);
-    };
+  useEffect(() => {
+    if (grade !== "") {
+      setMetadataWrapperState(" containing");
+    } else {
+      setMetadataWrapperState("");
+    }
+  }, [grade]);
 
-    const handleOnBlurInput = () => {
-        setFocusInput(false);
-        setIsSaving(true);
-        if (grade !== "") {
-            setMetadataWrapperState(" containing");
-        } else {
-            setMetadataWrapperState("");
-        }
+  const handleOnClickMetadataWrapper = () => {
+    setFocusInput(true);
+  };
 
-        putData(`/classrooms/${classId}/grades`, {
-            studentId: studentId,
-            gradeStructureId: gradeStructureId,
-            grade: grade,
-        })
-            .then(msg => {
-                mainContext.setReloadNeeded(true);
-                setIsSaving(false);
-            })
-            .catch(() => {
-                setIsSaving(false);
-                return message.error("Can't save grade");
-            });
-    };
+  const handleOnBlurInput = () => {
+    setFocusInput(false);
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        // @ts-ignore
-        if (isNaN(Number(e.target.value)) || (parseFloat(grade) > maximumGrade || parseFloat(grade) < 0)) {
-            setIsSaving(false);
-            return message.error("Invalid grade");
-        } else {
-            setGrade(e.target.value);
-        }
+    if (gradeStr === "") {
+      setGradeStr(grade);
+      return;
     }
 
-    return (
-        <div className={`grade-board_grade-cell${focusInput ? " focus" : ""}`}>
-            {props.readOnly ? (
-                <div className="metadata-wrapper containing">
-                    <p className="current-grade">{grade}</p>
-                </div>
-            ) : (
-                <>
-                    {focusInput ? (
-                        <div className="input-wrapper">
-                            <input
-                                {...props}
-                                ref={inputRef}
-                                value={grade}
-                                onChange={e => handleInputChange(e)}
-                                onBlur={handleOnBlurInput}
-                                autoFocus={focusInput}
-                            />
-                            <p>/{maximumGrade}</p>
-                            <div className="underline" />
-                        </div>
-                    ) : (
-                        <div
-                            className={`metadata-wrapper${metaDataWrapperState}`}
-                            onClick={handleOnClickMetadataWrapper}
-                        >
-                            {metaDataWrapperState === " containing" ? (
-                                <>
-                                    <p className="current-grade">{grade}</p>
-                                    {isSaving && <p className="current-state">Saving...</p>}
-                                </>
-                            ) : (
-                                <p className="current-grade">____</p>
-                            )}
-                        </div>
-                    )}
-                </>
-            )}
+    if (
+      isNaN(Number(gradeStr)) ||
+      parseFloat(gradeStr) > maximumGrade ||
+      parseFloat(gradeStr) < 0
+    ) {
+      setIsSaving(false);
+      setGradeStr(grade);
+      return message.error("Invalid grade");
+    } else {
+      if (gradeStr === grade) {
+        return;
+      }
+
+      setIsSaving(true);
+
+      putData(`/classrooms/${classId}/grades`, {
+        studentId: studentId,
+        gradeStructureId: gradeStructureId,
+        grade: gradeStr,
+      })
+        .then(msg => {
+          setGrade(gradeStr);
+          mainContext.setReloadNeeded(true);
+          setIsSaving(false);
+        })
+        .catch(() => {
+          setIsSaving(false);
+          return message.error("Can't save grade");
+        });
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    if (
+      isNaN(Number(e.target.value)) ||
+      parseFloat(e.target.value) > maximumGrade ||
+      parseFloat(e.target.value) < 0
+    ) {
+      setIsSaving(false);
+      return message.error("Invalid grade");
+    } else {
+      setGrade(e.target.value);
+    }
+  };
+
+  return (
+    <div className={`grade-board_grade-cell${focusInput ? " focus" : ""}`}>
+      {props.readOnly ? (
+        <div className="metadata-wrapper containing">
+          <p className="current-grade">{grade}</p>
         </div>
-    );
+      ) : (
+        <>
+          {focusInput ? (
+            <div className="input-wrapper">
+              <input
+                {...props}
+                ref={inputRef}
+                value={gradeStr}
+                onChange={e => setGradeStr(e.target.value)}
+                onBlur={handleOnBlurInput}
+                autoFocus={focusInput}
+              />
+              <p>/{maximumGrade}</p>
+              <div className="underline" />
+            </div>
+          ) : (
+            <div
+              className={`metadata-wrapper${metaDataWrapperState}`}
+              onClick={handleOnClickMetadataWrapper}
+            >
+              {metaDataWrapperState === " containing" ? (
+                <>
+                  <p className="current-grade">{grade}</p>
+                  {isSaving && <p className="current-state">Saving...</p>}
+                </>
+              ) : (
+                <p className="current-grade">____</p>
+              )}
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
