df4d109e8f9b64e29d8f1dbc07c835357608d325
vominhtrieu <minhtrieuvo600@gmail.com>
2022-01-18 12:19:55 +0700

    Secure admin route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,35 +3,70 @@ import SignIn from "./auth/SignIn";
 import SignUp from "./auth/SignUp";
 import "./App.css";
 import Main from "./Main";
+import { MainContext } from "../contexts/main";
 import { RoutingContext } from "../contexts/routing";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Admin from "./admin/Admin";
+import io, { Socket } from "socket.io-client";
+import { getData } from "../handlers/api";
 
 function App() {
     const [requestedURL, setRequestedURL] = React.useState("/");
+    const [reloadNeeded, setReloadNeeded] = useState(true);
+    const [user, setUser] = useState({
+        id: 0,
+        name: "",
+        email: "",
+        studentId: "",
+        role: 1,
+    });
+    const [socket, setSocket] = useState<Socket>(
+        io(process.env.REACT_APP_API_HOST + "", {
+            auth: {
+                token: localStorage.getItem("token"),
+            },
+        })
+    );
+
+    useEffect(() => {
+        getData("/users/profile").then((user: any) => {
+            setUser(user);
+        });
+    }, []);
 
     return (
-        <RoutingContext.Provider value={{requestedURL: requestedURL, setRequestedURL: setRequestedURL}}>
-            <Router>
-                <Switch>
-                    <Route path="/signin">
-                        <SignIn />
-                    </Route>
-                    <Route path="/signup">
-                        <SignUp />
-                    </Route>
-                    <Route path="/admin/">
-                        <Admin />
-                    </Route>
-                    <Route path="/admin/*">
-                        <Admin />
-                    </Route>
-                    <Route path="*">
-                        <Main />
-                    </Route>
-                </Switch>
-            </Router>
-        </RoutingContext.Provider>
+        <MainContext.Provider
+            value={{
+                user: user,
+                setUser: setUser,
+                reloadNeeded: reloadNeeded,
+                setReloadNeeded: setReloadNeeded,
+                socket: socket,
+                setSocket: setSocket,
+            }}
+        >
+            <RoutingContext.Provider value={{requestedURL: requestedURL, setRequestedURL: setRequestedURL}}>
+                <Router>
+                    <Switch>
+                        <Route path="/signin">
+                            <SignIn />
+                        </Route>
+                        <Route path="/signup">
+                            <SignUp />
+                        </Route>
+                        <Route path="/admin/">
+                            <Admin />
+                        </Route>
+                        <Route path="/admin/*">
+                            <Admin />
+                        </Route>
+                        <Route path="*">
+                            <Main />
+                        </Route>
+                    </Switch>
+                </Router>
+            </RoutingContext.Provider>
+        </MainContext.Provider>
     );
 }
 
