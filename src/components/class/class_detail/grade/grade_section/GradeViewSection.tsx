90dcd545c9aea9796b9b3fb35b226a39e28a3ddb
vominhtrieu <minhtrieuvo600@gmail.com>
2022-01-20 11:33:10 +0700

    Calculate total score in student pages

--- src/components/class/class_detail/grade/grade_section/GradeViewSection.tsx
+++ src/components/class/class_detail/grade/grade_section/GradeViewSection.tsx
@@ -4,40 +4,52 @@ import { useParams } from "react-router-dom";
 import { getData } from "../../../../../handlers/api";
 import GradeViewCard from "../grade_card/GradeViewCard";
 
-export default function GradeViewSection({ teachers }: any) {
-  const [gradeDetails, setGradeDetails] = useState([]);
-  const { id } = useParams<any>();
+export default function GradeViewSection({teachers}: any) {
+    const [gradeDetails, setGradeDetails] = useState([]);
+    const {id} = useParams<any>();
 
-  useEffect(() => {
-    const fetchData = () => {
-      getData(`/classrooms/${id}/student/grades`)
-        .then(studentGradeDetails => {
-          setGradeDetails(studentGradeDetails);
-        })
-        .catch(() => message.error("Something went wrong!"));
-    };
+    useEffect(() => {
+        const fetchData = () => {
+            getData(`/classrooms/${id}/student/grades`)
+                .then(studentGradeDetails => {
+                    setGradeDetails(studentGradeDetails);
+                })
+                .catch(() => message.error("Something went wrong!"));
+        };
 
-    fetchData();
-  }, [id]);
+        fetchData();
+    }, [id]);
 
-  return (
-    <Space className="grade-section" direction="vertical">
-      {gradeDetails.length === 0 ? (
-        <Empty />
-      ) : (
-        gradeDetails.map((gradeDetail: any, index: number) => (
-          <GradeViewCard
-            key={index}
-            gradeDetailId={gradeDetail.gradeDetailId}
-            gradeStructureName={gradeDetail.gradeStructureName}
-            gradeStructureGrade={gradeDetail.gradeStructureGrade}
-            grade={gradeDetail.grade}
-            reviewState={gradeDetail.reviewState}
-            updatedDate={gradeDetail.updatedDate}
-            teachers={teachers}
-          />
-        ))
-      )}
-    </Space>
-  );
+    const calculateTotalScore = (): any => {
+        let total = 0;
+        gradeDetails.forEach(({grade, gradeStructureGrade}: any) => {
+            total += grade * gradeStructureGrade / 100;
+        });
+        return total;
+    }
+    return (
+        <Space className="grade-section" direction="vertical">
+            {gradeDetails.length === 0 ? (
+                <Empty />
+            ) : (
+                <>
+                    <h2 style={{marginBottom: 0, textAlign: "center"}}>Total:&nbsp;
+                        <span style={{fontWeight: "normal"}}>{calculateTotalScore()}</span></h2>
+                    {gradeDetails.map((gradeDetail: any, index: number) => (
+                        <GradeViewCard
+                            key={index}
+                            gradeDetailId={gradeDetail.gradeDetailId}
+                            gradeStructureName={gradeDetail.gradeStructureName}
+                            gradeStructureGrade={gradeDetail.gradeStructureGrade}
+                            grade={gradeDetail.grade}
+                            reviewState={gradeDetail.reviewState}
+                            updatedDate={gradeDetail.updatedDate}
+                            teachers={teachers}
+                        />
+                    ))
+                    }
+                </>
+            )}
+        </Space>
+    );
 }
