b15a6122b76ba6319108408f09c75668f84facd5
TranHoangDat <tranhoangdat75@gmail.com>
2022-01-20 11:14:11 +0700

    Add max length for comment input

--- src/components/class/class_detail/grade/grade_review_conversation/GradeReviewConversationFooter.tsx
+++ src/components/class/class_detail/grade/grade_review_conversation/GradeReviewConversationFooter.tsx
@@ -6,39 +6,42 @@ import { useParams } from "react-router-dom";
 import addGradeReviewCommentHandler from "../../../../../handlers/addGradeReviewCommentHandler";
 
 interface GradeReviewConversationFooterProps {
-    isFinal: boolean;
+  isFinal: boolean;
 }
 
-export default function GradeReviewConversationFooter({isFinal}: GradeReviewConversationFooterProps) {
-    const {id, gradeDetailId} = useParams<any>();
-    const [comment, setComment] = useState("");
+export default function GradeReviewConversationFooter({
+  isFinal,
+}: GradeReviewConversationFooterProps) {
+  const { id, gradeDetailId } = useParams<any>();
+  const [comment, setComment] = useState("");
 
-    const handleOnClick = async () => {
-        if (comment === "") {
-            return message.info("Please input your comment!");
-        }
-        await addGradeReviewCommentHandler(id, gradeDetailId, comment);
-        setComment("");
-    };
+  const handleOnClick = async () => {
+    if (comment === "") {
+      return message.info("Please input your comment!");
+    }
+    await addGradeReviewCommentHandler(id, gradeDetailId, comment);
+    setComment("");
+  };
 
-    return (
-        <footer className="grade-review-conversation_footer">
-            <Avatar icon={<UserOutlined />} />
-            <Input.Group>
-                <Input.TextArea
-                    value={comment}
-                    onChange={e => setComment(e.target.value)}
-                    placeholder="Add private comment"
-                    disabled={isFinal}
-                    autoSize
-                />
-                <Button
-                    shape="circle"
-                    icon={<SendOutlined />}
-                    onClick={handleOnClick}
-                    disabled={isFinal}
-                />
-            </Input.Group>
-        </footer>
-    );
+  return (
+    <footer className="grade-review-conversation_footer">
+      <Avatar icon={<UserOutlined />} />
+      <Input.Group>
+        <Input.TextArea
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+          placeholder="Add private comment"
+          disabled={isFinal}
+          maxLength={255}
+          autoSize
+        />
+        <Button
+          shape="circle"
+          icon={<SendOutlined />}
+          onClick={handleOnClick}
+          disabled={isFinal}
+        />
+      </Input.Group>
+    </footer>
+  );
 }
